095748be3c8bd557b86345b038e818b96ad9bc3c
Yut Fut <seregey.golubev@mail.ru>
2024-09-01 14:59:50 +0300

    edited

--- internal/oltp/oltp_worker/useCase/useCase.go
+++ internal/oltp/oltp_worker/useCase/useCase.go
@@ -2,6 +2,8 @@ package usecase
 
 import (
 	"context"
+	"log"
+	"time"
 
 	"etl/internal/oltp/oltp_worker/repository"
 )
@@ -13,6 +15,9 @@ type UseCase interface {
 type useCase struct {
 	repository repository.Repository
 	logger *log.Logger
+
+	lastInsertID uint64
+	lastUpdateAT time.Time
 }
 
 func NewUseCase(
@@ -22,13 +27,16 @@ func NewUseCase(
 	return &useCase{
 		repository: repository,
 		logger: logger,
+
+		LastInsertID: 0,
+		LastUpdateAT: time.Date(1970, 01, 01, 0, 0, 0),
 	}
 }
 
 func (u *useCase) Start(ctx context.Context) {
 	_, err := u.repository.SelectMeta(ctx)
 	if err != nil {
-		logger.Println("Start ::: u.repository.SelectMeta ::: %v", err)
+		u.logger.Println("Start ::: u.repository.SelectMeta ::: %v", err)
 	}
 }
 
